171b21226d038664838e868e9f0d07ed120798e8
Koushik2208 <gorrekoushik@gmail.com>
2025-11-12 23:58:50 +0530

    error fixes

--- lib/error.ts
+++ lib/error.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
-
-import { RequestError, ValidationError } from "../http-errors";
-import logger from "../logger";
+import { RequestError, ValidationError } from "./http-errors";
+import logger from "./logger";
 
 export type ResponseType = "api" | "server";
 
